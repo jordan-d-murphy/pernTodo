f3b90ed8476eb13da1362a0974a0cb8acf268c96
Jordan Murphy <35613487+jordan-d-murphy@users.noreply.github.com>
2024-04-12 01:17:11 -0600

    react create

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+// components
+import InputToDo from './components/InputTodo';
+
 function App() {
   return (
-    <Fragment></Fragment>
+    <Fragment>
+      <div className="container">
+        <InputToDo />
+      </div>      
+    </Fragment>
   );
 }
 
